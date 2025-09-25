1a866105c0f69408fe163e013071129eb0eb3ab3
krishna <nextkrishna100@gmail.com>
2025-09-25 10:04:50 +0530

    ec2 push

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIARZ5BNAAHXUCHTDAL"
+  secret_key = "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"
+  
+}
+
+resource "instance_type" "ec2_instance" {
+  instance_type = "t2.micro"
+    ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI    
+
+}
