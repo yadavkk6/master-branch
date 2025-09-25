302298d7d937b572143c6d5be6c80804a404be94
krishna <nextkrishna100@gmail.com>
2025-09-25 10:14:06 +0530

    puch to github

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIARZ5BNAAHXUCHTDAL"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   
 }
 
